3210960af7a1f3271ce7bd322c2791d1911b2763
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-07-15 18:17:37 -0400

    separated get and options request to make sure model isnt run multiple times added flask cache to cache the result of the model and prevent continuos calls to predict

--- api/server.py
+++ api/server.py
@@ -1,7 +1,13 @@
 from flask import Flask, Response, jsonify
+from flask_caching import Cache
 from lib import model_handler
 
+config = {"CACHE_TYPE": "SimpleCache", "CACHE_DEFAULT_TIMEOUT": 300}
+
 app = Flask(__name__)
+
+app.config.from_mapping(config)
+cache = Cache(app)
 
 host = "http://192.168.2.210"
 port = 3000
@@ -9,12 +15,22 @@ port = 3000
 
 @app.route("/")
 def index():
-    return {"text": "Hello"}
+    return {"text": "hello"}
 
 
-@app.route("/predict/<ticker>", methods=["get", "options"])
+@app.route("/predict/<ticker>", methods=["options"])
+def options_prediction(ticker: str):
+    resp = Response(status=200)
+    resp.headers["Access-Control-Allow-Origin"] = f"{host}:{port}"
+    resp.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return resp
+
+
+@app.route("/predict/<ticker>", methods=["get"])
+@cache.cached()
 def prediction(ticker: str):
     prediction = model_handler.get_prediction_for(ticker)
+    prediction = "{:.2f}".format(prediction)
     resp = jsonify({"price": prediction})
     resp.headers["Access-Control-Allow-Origin"] = f"{host}:{port}"
     resp.headers["Access-Control-Allow-Headers"] = "Content-Type"
